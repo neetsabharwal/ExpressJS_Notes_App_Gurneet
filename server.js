c087d867caec22d8eae8ad46abf9e90b5ef99ce0
Gurneet <gsingh2@atb.com>
2022-07-20 08:58:50 -0400

    add heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const uniqid = require('uniqid');
 const express = require('express');
 const app = express();
-const PORT = process.argv.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const { notes } = require('./db/db.json');
 
 //middleware
